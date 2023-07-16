dc5fd4f81e1614b964c62841572387f74e185b6d
neo0177 <103036977+neo0177@users.noreply.github.com>
2023-07-16 21:02:17 +0530

    on learning

--- Py/MultiTable.py
+++ Py/MultiTable.py
@@ -3,8 +3,9 @@ num = int(input("Enter the number ? " ))
 for i in range(1,11) :
     mul = num * i 
     print(num, "*", i, "=", mul) 
-
-# # n = 1
+    
+    
+# #  n = 1
 # # while n
 # i = 1
 # for i in range(1,10) :
